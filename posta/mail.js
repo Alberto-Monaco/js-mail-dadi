558fa18c351eaf3b23a0de76558edb7887397df6
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-03 13:47:20 +0200

    complete exercise mail

---
+++ posta/mail.js
@@ -0,0 +1,21 @@
+//*Mail
+//*Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+let list_mail = [
+	'alberto@yahoo.com',
+	'gennaro@libero.it',
+	'mario@gmail.com',
+	'manuel@gmail.com',
+	'terasa@libero.it',
+	'carlo@gmail.com',
+	'gennaro@gmail.com',
+	'maria@yahoo.com'
+]
+
+let mail_user = prompt('Quale è la tua email')
+
+if (list_mail.includes(mail_user)) {
+	alert('puoi passare')
+} else {
+	alert('non puoi passare')
+}
